a8b920d81574bc98ad11f98e93b924505433c677
Rafael Ricci <rlopes.ricci@gmail.com>
2020-11-01 14:40:21 -0300

    Add routes to MVC structure

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\RepositoryController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::resource('tags', TagController::class)->except([
+    'show'
+]);
+Route::resource('repositories', RepositoryController::class)->only([
+    'index', 'show', 'store', 'destroy'
+]);
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
