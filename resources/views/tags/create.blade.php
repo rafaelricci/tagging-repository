39730c3144ef79602db2109aecc1c0afba16ae94
Rafael Ricci <rlopes.ricci@gmail.com>
2020-11-01 14:42:26 -0300

    Add and refactor view to structure MVC

---
+++ resources/views/tags/create.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container bg-white py-4">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <h2>
+                Adicionar nova Tag 
+                <a href="{{ route('tags.index') }}" class="btn btn-sm btn-primary">
+                    Voltar para listagem
+                </a>
+            </h2>
+            @include('layouts.flashMessage')
+            <form action="{{ route('tags.store') }}" method="POST">
+                @include('tags.form', ['tag' => $tag])
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/tags/index.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container bg-white py-4">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <h2>
+                Listagem de Tags 
+                <a href="{{ route('tags.create') }}" class="btn btn-sm btn-primary">
+                    Adicionar nova Tag
+                </a>
+            </h2>
+            @include('layouts.flashMessage')
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Título</th>
+                        <th scope="col" colspan="2"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($tags as $tag)
+                    <tr>
+                        <th scope="row">{{ $tag->id }}</th>
+                        <td>{{ $tag->title }}</td>
+                        <td>
+                            <a
+                                href="{{ route('tags.edit', ['tag' => $tag->id]) }}"
+                                class="btn btn-sm btn-primary">
+                                Editar
+                            </a>
+                        </td>
+                        <td>
+                            <form
+                                style="display: inline;"
+                                action="{{ route('tags.destroy', ['tag' => $tag->id]) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-sm btn-danger">Excluir</button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+        <div class="col-md-12">
+            {{ $tags->links() }}
+        </div>
+
+    </div>
+</div>
+@endsection
