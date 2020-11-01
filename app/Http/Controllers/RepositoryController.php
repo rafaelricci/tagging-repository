df9f242e72ae171e03d1d870a12e32229170db84
Rafael Ricci <rlopes.ricci@gmail.com>
2020-11-01 14:39:28 -0300

    Add and refactor controllers to MVC structure

---
+++ app/Http/Controllers/RepositoryController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Repository;
+use Illuminate\Http\Request;
+
+class RepositoryController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Repository $repository)
+    {
+        //
+    }
+
+    public function edit(Repository $repository)
+    {
+        //
+    }
+
+    public function update(Request $request, Repository $repository)
+    {
+        //
+    }
+
+    public function destroy(Repository $repository)
+    {
+        //
+    }
+}
