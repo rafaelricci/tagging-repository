e5b050efb53f311fd256f93fb8c03e1525efaa77
Rafael Ricci <rlopes.ricci@gmail.com>
2020-11-01 14:43:19 -0300

    Add migrations

---
+++ database/migrations/2020_11_01_140057_create_tags_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable(false)->unique();
+            $table->foreignId('user_id')
+                  ->nullable(false)
+                  ->constrained()
+                  ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
