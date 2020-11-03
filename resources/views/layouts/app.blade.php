314ff014ece655b98832e38d6a91a0c2ddc54433
Rafael Ricci <rlopes.ricci@gmail.com>
2020-11-03 08:04:29 -0300

    Add jscharting

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,11 +10,9 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link
-    rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css"
-    />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css" />
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
@@ -21,43 +20,45 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     TaggingRepository
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ml-auto">
                         @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('users.show', ['user' => Auth::user()->id]) }}">
+                                    Meu Perfil
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('users.show', ['user' => Auth::user()->id]) }}">
-                                        Meu Perfil
-                                    </a>
-
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                    {{ __('Logout') }}
+                                </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
                         @endguest
                     </ul>
                 </div>
@@ -67,57 +68,104 @@
         <div class="container-fluid">
             <div class="row">
                 @auth
-                    <div class="col-md-2">
-                        <ul class="nav flex-column py-4 bg-white border mt-4">
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('repositories.index') }}">
-                                    Repositórios
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('tags.index') }}">
-                                    Tags
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+                <div class="col-md-2">
+                    <ul class="nav flex-column py-4 bg-white border mt-4">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('repositories.index') }}">
+                                Repositórios
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('tags.index') }}">
+                                Tags
+                            </a>
+                        </li>
+                    </ul>
+                </div>
 
-                    <div class="col-md-10">
-                        <main class="py-4">
-                            @yield('content')
-                        </main>
-                    </div>
+                <div class="col-md-10">
+                    <main class="py-4">
+                        @yield('content')
+                    </main>
+                </div>
                 @else
-                    <div class="col-md-12">
-                        <main class="py-4">
-                            @yield('content')
-                        </main>
-                    </div>
+                <div class="col-md-12">
+                    <main class="py-4">
+                        @yield('content')
+                    </main>
+                </div>
                 @endauth
             </div>
         </div>
     </div>
+    <script src="https://code.jscharting.com/latest/jscharting.js"></script>
+    
+    <script>
+        var routeUsers = "{{ route('users.getPerDay') }}"
+        var resultUsers = [];
+        $users = fetch(
+            routeUsers
+        ).then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            return data.map(function (user) {
+                resultUsers.push({ name: user.date, y: user.quantity });
+            });
+        });
+
+        var chart = JSC.chart('chartDiv', { 
+            debug: true, 
+            type: 'column', 
+            title_label_text: 
+                'Histograma de novos usuários por dia', 
+            legend_visible: false, 
+            yAxis_defaultTick_label_text: '%value',
+            xAxis: { 
+                defaultTick: { 
+                placement: 'inside', 
+                label: { 
+                    color: 'white', 
+                    style: { 
+                        fontWeight: 'bold', 
+                        fontSize: 16 
+                    } 
+                } 
+                } 
+            }, 
+            series: [ 
+                { 
+                    defaultPoint: { 
+                        tooltip: 
+                        '<b>%yValue</b> se cadastraram<br> no dia <b>%name</b>', 
+                        label_text: '%value'
+                    }, 
+                    name: 'Users with access', 
+                    points: resultUsers
+                } 
+            ] 
+        }); 
+    </script>
+
     <script src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"></script>
     <script>
         var route = "{{ route('tags.getTags') }}"
         var multipleFetch = new Choices('#choices-multiple-remote-fetch', {
-          placeholder: true,
-          removeItemButton: true,
-          placeholderValue: 'Pick an Strokes record',
-          maxItemCount: 1,
-        }).setChoices(function() {
-          return fetch(
-            route
-          )
-            .then(function(response) {
-              return response.json();
-            })
-            .then(function(data) {
-              return data.map(function(tag) {
-                return { value: tag.id, label: tag.title };
-              });
+            placeholder: true,
+            removeItemButton: true,
+            placeholderValue: 'Pick an Strokes record',
+            maxItemCount: 1,
+        }).setChoices(function () {
+            return fetch(
+                route
+            ).then(function (response) {
+                    return response.json();
+            }).then(function (data) {
+                return data.map(function (tag) {
+                    return { value: tag.id, label: tag.title };
+                });
             });
         });
     </script>
 </body>
+
 </html>
