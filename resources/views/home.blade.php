fdfa9f42abc768e05c424e363c587a8ce39c50d1
Rafael Ricci <rlopes.ricci@gmail.com>
2020-11-03 08:03:05 -0300

    Add histograma users

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="card">
-    <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header">Bem vindo {{ Auth::user()->name }}!</div>
 
-    <div class="card-body">
-        @if (session('status'))
-        <div class="alert alert-success" role="alert">
-            {{ session('status') }}
+        <div class="card-body">
+            Está app permitira você criar Tags e associar aos repositórios buscados no GitHub!
         </div>
-        @endif
-
-        {{ __('You are logged in!') }}
     </div>
+</div>
+
+<div class="col-md-12 mt-3" id="chartDiv">
 </div>
 @endsection
